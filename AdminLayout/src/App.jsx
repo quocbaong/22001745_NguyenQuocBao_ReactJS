d953bd079f3bab377366d74d9cfe13612999b2b5
QuocBao <nbao76296@gmail.com>
2025-04-11 00:09:40 +0700

    Cau 4 - RootLayout

--- AdminLayout/src/App.jsx
+++ AdminLayout/src/App.jsx
@@ -1,20 +1,22 @@
+// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
+import RootLayout from './pages/RootLayout';
 import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <Router>
-      <div className="flex min-h-screen">
-        <Sidebar />
-        
-        <div className="flex-1 bg-gray-50">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-          </Routes>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="projects" element={<div>Projects</div>} />
+          <Route path="teams" element={<div>Teams</div>} />
+          <Route path="analytics" element={<div>Analytics</div>} />
+          <Route path="messages" element={<div>Messages</div>} /> 
+          <Route path="integrations" element={<div>Integrations</div>} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
---
+++ AdminLayout/src/pages/RootLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Sidebar from '../components/Sidebar';
+import { Outlet } from 'react-router-dom';
+
+const RootLayout = () => {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex-1 bg-gray-50 p-4">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default RootLayout;
